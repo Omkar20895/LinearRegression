b9bacfaa2d0e11206c36656ecabedb720cb6f312
Omkar Reddy <omkarreddy@Omkars-MacBook-Pro-3.local>
2019-07-26 16:19:40 -0400

    Updating the gradient descent algorithm

--- linearRegression.py
+++ linearRegression.py
@@ -1,39 +1,51 @@
 import numpy as np
 import pandas as pd
+import matplotlib as plt
 
 class LinearRegression():
 
     weights = list()
-    alpha = 0.01
+    alpha = 0.000001
    
     def get_data(self, data_frame, Y, X):
-        explanators = pd.DataFrame()
+        features = pd.DataFrame()
 
         predictor = data_frame[Y]
         for index in range(0, len(X)):
-            explanators[X[index]] = data_frame[X[index]]
+            features[X[index]] = data_frame[X[index]]
 
-        return predictor, explanator
+        return predictor, features
 
-    def optimise_weights(self, observations, predictor, explanators):
+    def optimize_weights(self, predictor, features):
         diff = 10
         iterations = 10000
         while(iterations):
-            for index in range(0, len(weights)):
-                partial_derivative = 
-            prev_coeff = coeff
-            coeff = prev_coeff - alpha*partial_derivative(error_func)
-            diff = prev_coeff - coeff
-            iteration -= 1
+            total_sum = np.zeros(len(features.index))
+            feature_sum = 0
+            for index in range(0, len(self.weights)-1):
+                total_sum += features[features.columns[index]] * self.weights[index]
+            total_sum += np.ones(len(features.index)) * self.weights[-1]
+            error_diff = predictor - total_sum
+            
+            for index in range(0, len(self.weights)-1):
+                partial_diff = error_diff.dot(features[features.columns[index]])
+                partial_diff_sum = partial_diff.sum()
+                partial_diff = partial_diff_sum/float(len(features.index))
+                self.weights[index] = self.weights[index] - self.alpha*partial_diff_sum
+
+            self.weights[-1] = self.weights[-1] - self.alpha*(error_diff.sum()/float(len(features.index)))
+            print(self.weights)
+            iterations -= 1
     
     def train_linear_model(self, df, Y, X):
-        predictor, explanator = self.get_data(df, Y, X) 
-        weights = list(np.random.randn(len(kwargs)))
+        predictor, features = self.get_data(df, Y, X) 
+        self.weights = list(np.random.randn(len(X)+1))
 
-        self.optimize_weights(predictor, explanator)
+        self.optimize_weights(predictor, features)
+        #self.optimize_weights(predictor, explanator)
     
     def get_predictions(self):
-        
+        print("getting individual data points")
         
     def get_individual_predictions(self, kwargs):
         result = 0
@@ -45,6 +57,7 @@ class LinearRegression():
         
         return result
 
+data_frame = pd.DataFrame(np.random.randint(0, 100, size=(100, 2)), columns=["X", "Y"])
 
 LR = LinearRegression()
-LR.train_linear_model()
+LR.train_linear_model(data_frame, "Y", ["X"])
